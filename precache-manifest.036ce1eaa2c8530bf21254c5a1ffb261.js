3a1eaf3b3b5a56ecba898d383e7f7a1cd65e291f
Ryan o'Hare <ryan.1.ohare@gmail.com>
2019-11-20 20:30:10 +0000

    Updates

---
+++ precache-manifest.036ce1eaa2c8530bf21254c5a1ffb261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68889ec820a913fe760d11e8a5ec17d2",
+    "url": "/deployedColorPalette/index.html"
+  },
+  {
+    "revision": "cec6e890bc767ba97644",
+    "url": "/deployedColorPalette/static/css/2.5774dd83.chunk.css"
+  },
+  {
+    "revision": "1e6f480931af400f9f2f",
+    "url": "/deployedColorPalette/static/css/main.224ac33f.chunk.css"
+  },
+  {
+    "revision": "cec6e890bc767ba97644",
+    "url": "/deployedColorPalette/static/js/2.d67da587.chunk.js"
+  },
+  {
+    "revision": "1e6f480931af400f9f2f",
+    "url": "/deployedColorPalette/static/js/main.916a4573.chunk.js"
+  },
+  {
+    "revision": "34b236e940c4f0409866",
+    "url": "/deployedColorPalette/static/js/runtime-main.110f6c73.js"
+  },
+  {
+    "revision": "b004491070b902a916ac7925780146bb",
+    "url": "/deployedColorPalette/static/media/bg.b0044910.svg"
+  }
+]);
